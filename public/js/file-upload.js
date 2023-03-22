f5151afc37b5a93912ee7a3ef1d5368419a0f351
sikakaleb <115256206+sikakaleb@users.noreply.github.com>
2023-03-22 09:04:28 +0100

    Add files via upload

---
+++ public/js/file-upload.js
@@ -0,0 +1,20 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*************************************!*\
+  !*** ./resources/js/file-upload.js ***!
+  \*************************************/
+(function ($) {
+  'use strict';
+
+  $(function () {
+    $('.file-upload-browse').on('click', function () {
+      var file = $(this).parent().parent().parent().find('.file-upload-default');
+      file.trigger('click');
+    });
+    $('.file-upload-default').on('change', function () {
+      $(this).parent().find('.form-control').val($(this).val().replace(/C:\\fakepath\\/i, ''));
+    });
+  });
+})(jQuery);
+/******/ })()
+;
---
+++ public/js/off-canvas.js
@@ -0,0 +1,16 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************************!*\
+  !*** ./resources/js/off-canvas.js ***!
+  \************************************/
+(function ($) {
+  'use strict';
+
+  $(function () {
+    $('[data-toggle="offcanvas"]').on("click", function () {
+      $('.sidebar-offcanvas').toggleClass('active');
+    });
+  });
+})(jQuery);
+/******/ })()
+;
